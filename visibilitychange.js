e5f69ebacdaba4ed4a7089eeef54804f4de0125f
fox <626269256@qq.com>
2016-06-04 10:04:35 +0800

    修改 撤回消息装逼.html

--- visibilitychange.js
+++ visibilitychange.js
@@ -7,8 +7,10 @@ var titleTime, oldTitle = document.title;
 document.addEventListener('visibilitychange',function (){
     if(document.hidden){
         document.title = '(●—●)咦，去哪儿啊？)';
+        document.getElementById('shortcut').href = 'http://7xon9l.com1.z0.glb.clouddn.com/fail.ico';
     }else{
         document.title = '(/≧▽≦/)欢迎回来！' + oldTitle;
+        document.getElementById('shortcut').href = 'http://dengxiaozhen.github.io/favicon.ico';
         titleTime = setTimeout(function (){
             document.title = oldTitle;
         },2000);
